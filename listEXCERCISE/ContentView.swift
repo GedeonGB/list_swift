2acacf7417dc54ddd921458c2a4aa22c8944421d
GedeonGB <gedeonbaraka1@gmail.com>
2023-02-23 11:10:45 +0100

    Initial Commit

---
+++ listEXCERCISE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  listEXCERCISE
+//
+//  Created by Mac on 2/23/23.
+//  Copyright © 2023 Mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
