508ef657f861129e875d95fe175730ec0985bd5f
GedeonGB <gedeonbaraka1@gmail.com>
2023-03-01 16:13:48 +0100

    just initialized the sermon list to be fetched in the list

---
+++ listEXCERCISE/sermon.swift
@@ -0,0 +1,14 @@
+//
+//  sermon.swift
+//  listEXCERCISE
+//
+//  Created by Mac on 3/1/23.
+//  Copyright © 2023 Mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct sermon: Identifiable {
+    let id=UUID()
+}
+
